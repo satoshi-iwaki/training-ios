bb70cb5060c66c133077672741a955148f5b6872
Satoshi Iwaki <satoshi.iwaki@gmail.com>
2017-07-27 00:13:12 +0900

    Add RSS reader

---
+++ RssReader/RssReader/RssReader/RssFeed.swift
@@ -0,0 +1,13 @@
+//
+//  RssFeed.swift
+//  RssReader
+//
+//  Created by Iwaki Satoshi on 2017/07/26.
+//  Copyright © 2017年 Satoshi Iwaki. All rights reserved.
+//
+
+import UIKit
+
+class RssFeed: NSObject {
+
+}
---
+++ RssReader/RssReader/RssReader/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  RssReader
+//
+//  Created by Iwaki Satoshi on 2017/07/26.
+//  Copyright © 2017年 Satoshi Iwaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, URLSessionDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    private let feedUrl = URL(string: "https://rss.itunes.apple.com/api/v1/us/apple-music/new-music/10/explicit/json")
+    private let feeds = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    //MARK: UITableViewDataSource
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feeds.count;
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell", for: indexPath)
+//        cell.imageView!.image
+        
+    }
+
+    //MARK: UITableViewDelegate
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+
+}
+
