0946dfbc512c54a1ccffd6f7322dfd31099e9a9e
Satoshi Iwaki <satoshi.iwaki@gmail.com>
2018-04-09 22:42:39 +0900

    Add unit tests

--- Calculator/Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/Calculator/CalculatorTests/CalculatorTests.swift
@@ -21,16 +21,42 @@ class CalculatorTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAdding() {
+        struct Parameter {
+            let digits1: [String]
+            let digits2: [String]
+            let expect1: NSDecimalNumber
+            let expect2: String
+        }
+        let parameters: [Parameter] = [
+            Parameter(digits1: ["0"], digits2: ["0"], expect1: 0, expect2: "0"),
+            Parameter(digits1: ["0"], digits2: ["1"], expect1: 1, expect2: "1"),
+            Parameter(digits1: ["1"], digits2: ["0"], expect1: 1, expect2: "1"),
+            Parameter(digits1: ["1"], digits2: ["2"], expect1: 3, expect2: "3"),
+            Parameter(digits1: ["1", "2"], digits2: ["2", "3"], expect1: 35, expect2: "35"),
+            ]
+        for parameter in parameters {
+            let calculator = Calculator()
+            for digit1 in parameter.digits1 {
+                calculator.inputDigit(by: digit1)
+            }
+            calculator.inputOperator(.adding)
+            for digit2 in parameter.digits2 {
+                calculator.inputDigit(by: digit2)
+            }
+            XCTAssertEqual(calculator.caluculate(), parameter.expect1)
+            XCTAssertEqual(calculator.display, parameter.expect2)
         }
     }
+
+    func testSubtracting() {
+        let calculator = Calculator()
+        calculator.inputDigit(by: "1")
+        calculator.inputOperator(.subtracting)
+        calculator.inputDigit(by: "2")
+        XCTAssertEqual(calculator.caluculate(), -1)
+        XCTAssertEqual(calculator.display, "-1")
+    }
+
     
 }
